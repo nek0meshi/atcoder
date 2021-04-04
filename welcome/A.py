1a83ecc8d1c0cc8a263076bcb952913c4f481f0d
nek0meshi <nekmeshi@gmail.com>
2021-04-04 15:32:00 +0900

    welcome

---
+++ welcome/A.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+a = int(input())
+b, c = map(int, input().split())
+s = input()
+
+print(a + b + c, s)
+
+print("{} {}".format(a+b+c, s))
+
+"""
+input1
+1
+2 3
+test
+
+output1
+6 test
+
+
+input2
+72
+128 256
+myonmyon
+
+
+output2
+72
+128 256
+myonmyon
+
+"""
